17f62f3c32cda8390584e4c51381d7e8ab073e14
Esley <esleynathan@hotmail.com>
2023-07-26 16:37:22 -0300

    Finance | Extrato novo valor

--- extrato/views.py
+++ extrato/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from perfil.models import Categoria, Conta
+from .models import Valores
 
 # Create your views here.
 def novo_valor(request):
@@ -7,3 +8,35 @@ def novo_valor(request):
         contas = Conta.objects.all()
         categorias = Categoria.objects.all() 
         return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
+    elif request.method == "POST":
+        valor = request.POST.get('valor')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        conta = request.POST.get('conta')
+        tipo = request.POST.get('tipo')
+        
+        valores = Valores(
+            valor=valor,
+            categoria_id=categoria,
+            descricao=descricao,
+            data=data,
+            conta_id=conta,
+            tipo=tipo,
+        )
+
+        valores.save()
+
+        conta = Conta.objects.get(id=conta)
+
+        if tipo == 'E':
+            conta.valor += int(valor)
+        else:
+            conta.valor -= int(valor)
+
+        conta.save()
+
+        
+
+        messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+        return redirect('/extrato/novo_valor')
