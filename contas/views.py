5d7ca5655c1fa4b804ea3f76938bf709f3a275eb
Esley <esleynathan@hotmail.com>
2023-08-04 15:22:48 -0300

    Finance | Dashboards

--- contas/views.py
+++ contas/views.py
@@ -43,5 +43,7 @@ def ver_contas(request):
     
     restantes = contas.exclude(id__in=contas_vencidas).exclude(id__in=contas_pagas).exclude(id__in=contas_proximas_vencimento)
 
+    print(contas_vencidas)
+    
     return render(request, 'ver_contas.html', {'contas_vencidas': contas_vencidas, 'contas_proximas_vencimento': contas_proximas_vencimento, 'restantes': restantes})
     
--- perfil/views.py
+++ perfil/views.py
@@ -4,6 +4,8 @@ from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
 from .utils import calcula_total
+from extrato.models import Valores
+from django.db.models import Sum
 
 def home(request):
     contas = Conta.objects.all()
@@ -76,3 +78,12 @@ def update_categoria(request, id):
     categoria.save()
 
     return redirect('/perfil/gerenciar/')
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+
+    for categoria in categorias:
+        dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
